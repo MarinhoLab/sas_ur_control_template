734e3308468cbee785c8a63bb427b801eadc6cae
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2024-11-26 13:36:23 +0000

    [launch] Re-organizing the launch files to minimize the code repetition and the necessary terminal window count.

--- launch/dummy_move_in_coppeliasim_example_launch.py
+++ launch/dummy_move_in_coppeliasim_example_launch.py
@@ -12,19 +12,14 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
-    dummy_robot_example_launch = IncludeLaunchDescription(
+    dummy_with_coppeliasim_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('sas_ur_control_template'), 'launch'),
-            '/dummy_robot_example_launch.py'])
+            '/dummy_with_coppeliasim_launch.py'])
     )
-    composed_with_coppeliasim_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('sas_ur_control_template'), 'launch'),
-            '/composed_with_coppeliasim_launch.py'])
-    )
+
     return LaunchDescription([
-        dummy_robot_example_launch,
-        composed_with_coppeliasim_launch,
+        dummy_with_coppeliasim_launch,
         Node(
             package='sas_ur_control_template',
             executable='joint_interface_example.py',
