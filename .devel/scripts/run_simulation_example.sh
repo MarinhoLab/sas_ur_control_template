6f4abd69b4476918587d36cceefe71ab95e80556
Murilo M. Marinho <murilo.marinho@manchester.ac.uk>
2025-06-26 21:26:18 +0100

    [docker] Adding test vnc server

--- .devel/scripts/run_simulation_example.sh
+++ .devel/scripts/run_simulation_example.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
+# Adding vnc information
+## https://www.baeldung.com/linux/docker-container-gui-applications
+apt-get update && apt-get install -qqy x11-apps x11vnc xvfb
+mkdir ~/.vnc
+x11vnc -storepasswd 1234 ~/.vnc/passwd
+
 # Run CoppeliaSim, start simulation, and auto quit
 # https://manual.coppeliarobotics.com/en/commandLine.htm
 cd "$COPPELIASIM_PATH"
-./coppeliaSim.sh -s5000 -q \
+./coppeliaSim.sh -s -q \
 "$HOME"/sas_tutorial_workspace/src/sas_ur_control_template/scenes/sas_UR3_470rev4.ttt &
 
 # Run simulation launch file
---
+++ .devel/scripts/setup_enviroment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+source /opt/ros/jazzy/setup.bash
+cd "$HOME"/sas_tutorial_workspace
+cd ~/sas_tutorial_workspace/
+colcon build
