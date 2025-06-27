f101494d6ce4fab89b878c5e3b0ca5ac542b133e
Murilo M. Marinho <murilo.marinho@manchester.ac.uk>
2025-06-27 20:31:04 +0100

    [setup_enviroment] Adding basic example alias.

--- .devel/scripts/setup_environment.sh
+++ .devel/scripts/setup_environment.sh
@@ -6,8 +6,23 @@ cd "$HOME"/sas_tutorial_workspace
 colcon build
 
 # Add sas_tutorial_workspace to bash
-echo "source $HOME/sas_tutorial_workspace/install/setup.bash" >> ~/.bashrc
+echo "source $HOME/sas_tutorial_workspace/install/setup.bash"\
+>> ~/.bashrc
 
-# Setup example path
-echo "export SAS_UR_CONTROL_TEMPLATE_PATH='${HOME}/sas_tutorial_workspace/src/sas_ur_control_template'">> ~/.bashrc
+# Setup ROS 2 package path
+SAS_UR_CONTROL_TEMPLATE_PATH="${HOME}/sas_tutorial_workspace/src/sas_ur_control_template"
+
+echo "export SAS_UR_CONTROL_TEMPLATE_PATH='${SAS_UR_CONTROL_TEMPLATE_PATH}'"\
+>> ~/.bashrc
+
+# Setup example aliases
+UR3E_BASIC_EXAMPLE="${SAS_UR_CONTROL_TEMPLATE_PATH}/.devel/scripts/run_simulation_example.sh"
+chmod +x "${UR3E_BASIC_EXAMPLE}"
+
+echo "alias ur3e_basic_example='${UR3E_BASIC_EXAMPLE}'"\
+>> ~/.bashrc
+
+
+
+
 
