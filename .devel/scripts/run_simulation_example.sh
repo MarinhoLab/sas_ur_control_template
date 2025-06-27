0cce139acd60b640c88499f5245abbdeec9249fb
Murilo M. Marinho <murilo.marinho@manchester.ac.uk>
2025-06-27 22:47:54 +0100

    [docker] Adjusting timings

--- .devel/scripts/run_simulation_example.sh
+++ .devel/scripts/run_simulation_example.sh
@@ -5,13 +5,13 @@ set -e
 # https://manual.coppeliarobotics.com/en/commandLine.htm
 cd "$COPPELIASIM_PATH"
 ./coppeliaSim.sh \
--s60000 \
+-s120000 \
 -q \
 "$SAS_UR_CONTROL_TEMPLATE_PATH"/scenes/UR3e_480rev0.ttt &
 
 # Sleep a bit so that Coppeliasim can load the simulation
 echo "Giving CoppeliaSim some time to relax."
-sleep 30
+sleep 45
 
 # Run simulation launch file
 source "$HOME"/sas_tutorial_workspace/install/setup.bash
