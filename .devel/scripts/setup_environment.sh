b3b1124d9f33eaeca209f3fbd224e3b783eec391
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-06-30 21:40:59 +0100

    Update setup_environment.sh

--- .devel/scripts/setup_environment.sh
+++ .devel/scripts/setup_environment.sh
@@ -5,6 +5,8 @@ set -e
 cd "$HOME"/sas_tutorial_workspace
 source /opt/ros/jazzy/setup.bash
 colcon build
+rm -rf build
+rm -rf log
 
 # Add sas_tutorial_workspace to bash
 echo "source $HOME/sas_tutorial_workspace/install/setup.bash"\
