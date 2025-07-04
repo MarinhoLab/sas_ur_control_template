ed4403778f2e6d00578b590a00fa873123d1dd2c
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-07-04 23:17:15 +0100

    [docker] Removing docker image upload, using simpler composition.

--- .devel/scripts/setup_environment.sh
+++ .devel/scripts/setup_environment.sh
@@ -8,10 +8,6 @@ colcon build
 rm -rf build
 rm -rf log
 
-# Add sas_tutorial_workspace to bashrc
-echo "source $HOME/sas_tutorial_workspace/install/setup.bash"\
->> ~/.bashrc
-
 # Add sas_tutorial_workspace to bash_env
 echo "source $HOME/sas_tutorial_workspace/install/setup.bash"\
 >> /etc/bash_env
