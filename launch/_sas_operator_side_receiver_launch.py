db19539b987147929154c31061a7b040adc30184
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2025-07-14 10:59:19 +0100

    [docker] Added missing file

---
+++ launch/_sas_operator_side_receiver_launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        Node(
+            package='sas_operator_side_receiver',
+            executable='sas_operator_side_receiver_udp_node',
+            output='screen',
+            emulate_tty=True
+            parameters=[{
+                "patient_side_ips": ["192.168.1.120"],
+                "patient_side_ports": [2223],
+                "operator_side_ports": [2223]
+            }]
+        )
+
+    ])
