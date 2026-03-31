ca781b2b1c26890073c9cb68e320a80d6fb0fcd5
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2026-03-31 13:56:34 +0100

    Create robot_example_cpp_launch.py

---
+++ launch/robot_example_cpp_launch.py
@@ -0,0 +1,30 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+def generate_launch_description():
+    real_robot_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('sas_ur_control_template'), 'launch'),
+            '/_robot_launch.py'])
+    )
+
+    return LaunchDescription([
+        real_robot_launch,
+        Node(
+            package='sas_ur_control_template',
+            executable='joint_interface_example_cpp',
+            output='screen',
+            emulate_tty=True,
+            name='sas_ur_control_template_joint_interface_example',
+            parameters=[{
+                "robot_topic_name": "ur_1"
+            }]
+        )
+    ])
