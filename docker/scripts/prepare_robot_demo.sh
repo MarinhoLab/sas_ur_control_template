36983ddb15a2e5625c2fe14e60ba88757f99f2fd
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-08-07 13:35:12 +0100

    Adjusting demo paths.

--- docker/scripts/prepare_robot_demo.sh
+++ docker/scripts/prepare_robot_demo.sh
@@ -5,5 +5,5 @@ set -e
 export FASTRTPS_DEFAULT_PROFILES_FILE=/root/sas_tutorial_workspace/src/sas_ur_control_template/docker/scripts/fastrtps_profile.xml
 
 # Show countdown
-cd /root/sas_tutorial_workspace/src/sas_ur_control_template/docker/scripts/
+cd /root/sas_tutorial_workspace/src/sas_ur_control_template/docker/scripts
 ./countdown.sh 10
