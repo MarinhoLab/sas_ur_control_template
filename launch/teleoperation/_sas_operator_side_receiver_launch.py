c2e61539a1cdc42b04a401fa2d5ac01d3afe5f3b
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-08-18 12:27:18 +0100

    Adjusting teleoperation

--- launch/teleoperation/_sas_operator_side_receiver_launch.py
+++ launch/teleoperation/_sas_operator_side_receiver_launch.py
@@ -2,10 +2,7 @@ import os
 import sys
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-
 
 _REQUIRED_ENV_VARS = ['CONTROL_PC_IP', 'PATIENT_SIDE_PORT', 'OPERATOR_SIDE_PORT']
 
@@ -17,13 +14,7 @@ def generate_launch_description():
                   f"Please set it in docker/.env or export it before launching.", file=sys.stderr)
             sys.exit(1)
 
-    patient_side_ips = LaunchConfiguration('patient_side_ips')
-
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'patient_side_ips',
-            default_value=f"[{os.environ['CONTROL_PC_IP']}]"
-        ),
         Node(
             package='sas_operator_side_receiver',
             executable='sas_operator_side_receiver_udp_node',
@@ -31,7 +22,7 @@ def generate_launch_description():
             emulate_tty=True,
             name='sas_operator_side_receiver_udp_node',
             parameters=[{
-                "patient_side_ips": patient_side_ips,
+                "patient_side_ips": f"[{os.environ['CONTROL_PC_IP']}]",
                 "patient_side_ports": [int(os.environ['PATIENT_SIDE_PORT'])],
                 "operator_side_ports": [int(os.environ['OPERATOR_SIDE_PORT'])]
             }]
